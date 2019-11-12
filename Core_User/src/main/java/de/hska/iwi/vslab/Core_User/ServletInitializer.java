8656ce6baff39e44b742cdf30d4b7f6b852f05f4
and1kl <40032530+and1kl@users.noreply.github.com>
2019-11-12 18:35:17 +0100

    Spring Boot Project Initial

---
+++ Core_User/src/main/java/de/hska/iwi/vslab/Core_User/ServletInitializer.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Core_User;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CoreUserApplication.class);
+	}
+
+}
---
+++ Core_User/src/test/java/de/hska/iwi/vslab/Core_User/CoreUserApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Core_User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
